479b24a4560bf546e943d89d15bad3cc7a161ba9
Côme Chevallier <come.chevallier@icloud.com>
2023-05-25 09:53:10 +0200

    adding global variable

--- src/components/ButtonAuthProvider.jsx
+++ src/components/ButtonAuthProvider.jsx
@@ -5,7 +5,9 @@ import { signIn } from "next-auth/react";
 export default function ButtonAuthProvider({ name, id }) {
   return (
     <button
-      onClick={() => signIn(id, { callbackUrl: "http://localhost:3000/" })}
+      onClick={() =>
+        signIn(id, { callbackUrl: process.env.NEXT_PUBLIC_NEXTAUTH_URL + "/" })
+      }
       className="bg-red-400 rounded-lg p-3 my-3 text-white hover:bg-red-500 duration-200 transition"
     >
       Sign in with {name}
